7b90371b51079bea5273e6c0b79fb17f82a3f017
Chris Carr <krzysztof@Chriss-MacBook-Pro.local>
2019-10-18 16:37:32 -0400

    Polly front end set up

---
+++ polly-app/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+// import { Link, Switch, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      <nav></nav>
+      <main></main>
+    </div>
+  );
+}
+
+export default App;
