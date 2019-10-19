46eaf651c16a9c748a91b7b29b8fb7b4306167a3
lauren robinson <laurenrobinson@laurens-MacBook-Air.local>
2019-10-19 10:55:47 -0700

    poll name created

--- polly-app/src/Components/Create.js
+++ polly-app/src/Components/Create.js
@@ -1,15 +1,41 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Create.css";
 import { Link, Route } from "react-router-dom";
 
 class Create extends Component {
-render(){
-    return(
-        <div>
-            <h1>Create a new poll</h1>
-        </div>
-    )
-}
+  constructor(props) {
+    super(props);
+    this.state = { value: "" };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
+  handleSubmit(event) {
+    console.log("poll submitted " + this.state.value);
+    event.preventDefault();
+  }
+  render() {
+    return (
+      <div>
+        <h1>Create a new poll</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Poll Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+              name="pollName"
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
 }
 
-export default Create
+export default Create;
