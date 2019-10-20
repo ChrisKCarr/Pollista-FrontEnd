fb909915270223f37a17cb06bbd55dbdabf0798f
Chris Carr <krzysztof@Chriss-MacBook-Pro.local>
2019-10-20 13:26:17 -0400

    Added Css files, working on making button functional

--- polly-app/src/Components/Home.js
+++ polly-app/src/Components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Link, Route } from "react-router-dom";
 import Poll from "./Poll";
+import "./Home.css";
 
 class Home extends Component {
   render() {
