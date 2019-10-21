cd5a8e3b24ef9efa02d7c06198e528f487f9c4bb
Thomas Ruffino <thomasruffino1@gmail.com>
2019-10-21 14:47:57 -0400

    added

---
+++ polly-app/src/Components/ChoiceList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react"
+import Counter from "./Counter"
+
+class ChoiceList extends Component {
+ 
+  render() { 
+    let choiceList = []
+    for (let i = 0; i < this.props.choice; i++) {
+      choiceList.push(<Choice />)
+    }
+    return <div className="Counter-row">{choiceList}</div>
+  }
+}
+
+export default ChoiceList;
