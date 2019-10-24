fa284582f757f9eea7e5b815a7171ed17b4a1040
Natalie Spencer <gnataliespen@gmail.com>
2019-10-24 16:59:16 -0500

    create page works

--- polly-app/src/Components/Create.js
+++ polly-app/src/Components/Create.js
@@ -2,52 +2,88 @@ import React, { Component } from "react";
 import "./Create.css";
 import { Route } from "react-router-dom";
 import Nav from "./Nav";
-import ChoiceList from "./ChoiceList";
+import "./ChoiceList.css";
+import "./Create.css";
+import "./ChoiceList.css";
 import PollContext from "../contexts/PollContext";
 
 class Create extends Component {
   static contextType = PollContext;
   constructor(props) {
     super(props);
-    this.state = { question: "", description: "", choices: [] };
-    this.i = 1;
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {
+      question: "",
+      description: "",
+      choices: [{ 0: "" }, { 1: "" }],
+    };
+    this.inputsNeeded = 2;
   }
-
+  addChoices = () => {
+    let choiceArr = this.state.choices;
+    let i = choiceArr.length;
+    for (i; i < this.inputsNeeded; i++) {
+      choiceArr.push({ [i]: "" });
+    }
+    this.setState({ choices: choiceArr });
+  };
+  renderInputList = () => {
+    let choiceArr = this.state.choices;
+    let inputs = [];
+    for (let i = 0; i < choiceArr.length; i++) {
+      inputs.push(
+        <li>
+          <input
+            type="text"
+            id={i}
+            name="choice"
+            value={choiceArr[i][i]}
+            onChange={e => this.handleChange(e)}
+          />
+        </li>,
+      );
+    }
+    return inputs;
+  };
   handleChange = event => {
-    let id = event.target.id;
-    return this.setState({ [id]: event.target.value });
+    if (event.target.name === "choice") {
+      let choiceArr = this.state.choices;
+      let id = event.target.id;
+      choiceArr[id][id] = event.target.value;
+      this.setState({ choices: [...choiceArr] });
+    } else {
+      this.setState({ [event.target.id]: event.target.value });
+    }
+  };
+  handlePlus = () => {
+    this.inputsNeeded += 1;
+    this.addChoices();
   };
   handleMinus = () => {
-    let choicesArr = this.state.choices;
-    choicesArr.pop();
-    this.i--;
-    this.setState({ choices: choicesArr });
+    if (this.inputsNeeded > 2) {
+      let choiceArr = this.state.choices;
+      choiceArr.pop();
+      this.inputsNeeded -= 1;
+      this.setState({ choices: [...choiceArr] });
+    }
   };
-  handleSubmit(event) {
+  handleSubmit = event => {
     event.preventDefault();
-    console.log(this.state);
-    //let choices =  this.state.choices.split
-    /* this.context.newPoll({
-     question: "What time do you wake up?",
-     choices: [
-       { text: "6am" },
-       { text: "8am" },
-       { text: "10am" },
-       { text: "noon" },
-     ],
-   });*/
-  }
+    let choiceArr = this.state.choices;
 
+    let poll = { question: this.state.question };
+    if (this.state.description !== "")
+      poll.description = this.state.description;
+    let choices = choiceArr.map((choice, i) => choice[i]);
+    poll.choices = choices;
+    this.context.newPoll(poll);
+    this.props.history.push("/");
+  };
   render() {
-    console.log(this.state);
-
     return (
       <div>
         <Route component={Nav} />
         <h1 className="Title">Create a new poll</h1>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={e => this.handleSubmit(e)}>
           <label>
             <h3>Question: </h3>
             <input
@@ -70,18 +106,27 @@ class Create extends Component {
             />
           </label>
           <br></br>
-
-          {/* <label>
-           <h3>Type: </h3>
-           <select>
-             <option>Food</option>
-             <option>Opinion</option>
-             <option>Question</option>
-           </select>
-         </label> */}
-          <br></br>
           <h3>Choices: </h3>
-          <ChoiceList choices={this.state.choices} />
+          <ul>{this.renderInputList()}</ul>
+          <div>
+            <div className="Counter-row">{this.state.choiceList}</div>
+            <div className="choiceButtonsContainer">
+              <button
+                className="choiceButton"
+                type="button"
+                onClick={this.handlePlus}
+              >
+                +
+              </button>
+              <button
+                className="choiceButton"
+                type="button"
+                onClick={this.handleMinus}
+              >
+                -
+              </button>
+            </div>
+          </div>
           <input type="submit" value="Submit" className="Button" />
         </form>
       </div>
