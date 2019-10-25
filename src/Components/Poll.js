1f786b8333424b3b0425da5c63f3b38c0e637786
Natalie Spencer <gnataliespen@gmail.com>
2019-10-25 13:16:09 -0500

    link color change

--- src/Components/Poll.js
+++ src/Components/Poll.js
@@ -5,13 +5,24 @@ import PollContext from "../contexts/PollContext";
 
 class Poll extends Component {
   static contextType = PollContext;
-
+  colorChange = () => {
+    if (this.context.user) {
+      if (this.context.user.votedOn.includes(this.props.pollObject._id)) {
+        return { color: "#d4d7dd" };
+      } else {
+        return { color: "rgb(86, 175, 226)" };
+      }
+    } else {
+      return { color: "rgb(86, 175, 226)" };
+    }
+  };
   render() {
     let poll = this.props.pollObject;
     return (
       <div className="PollContainer">
         <Link
           className="PollTitle"
+          style={this.colorChange()}
           to={{
             pathname: `/show/${this.props.pollObject._id}`,
             state: { poll },
